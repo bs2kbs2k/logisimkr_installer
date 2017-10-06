da4ec8fd7f3574a43f64d112c821d5bbf4a0edac
alswnsshrimp <alswnsguppy@gmail.com>
2017-10-06 21:51:43 +0900

    ㅎㅇㅎㅇ

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
-echo "# logisimkr_installer" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
+set /p string=커밋 내용을 입력하세요:
+git add *
+git commit -m "%string%"
 git remote add origin https://github.com/TPT-Logisim/logisimkr_installer.git
-git push -u origin master
+git push origin master
+
+워드 깔려있나요? 아뇨 잠깐만요
