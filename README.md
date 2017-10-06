32e77d5db3d0e0c9a06646a38c9af54455694e82
alswnsshrimp <alswnsguppy@gmail.com>
2017-10-06 21:36:15 +0900

    01

---
+++ README.md
@@ -0,0 +1,1 @@
+"# logisimkr_installer" 
---
+++ install.bat
@@ -0,0 +1,6 @@
+echo "# logisimkr_installer" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/TPT-Logisim/logisimkr_installer.git
+git push -u origin master
